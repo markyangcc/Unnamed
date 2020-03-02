8eeccb14a07910285495217630f41d5f0e876ed3
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-02 11:28:41 +0800

    Finish the program
    Character and integer numbers are different.
    color( ) function only accept character argument, not integer argument.
    It's a trap when you're not familiar with ASCII.

--- Celebrate New Year(words).c
+++ Celebrate New Year(words).c
@@ -1,35 +1,37 @@
+/*Rember the numbers in ASCII Table is not int numbers*/
+
 #include <stdio.h>
 #include <windows.h>
 
 #define N 10
 #define LINE 19
 
-//char RandBGColor(void);//Randomly generated background color
-//char RandFGColor(void);//Randomly generated foreground color
-char RandColor(void);
+char RandBGColor(void); //Randomly generated background color
+char RandFGColor(void); //Randomly generated foreground color
 void Delay(void); //Delay for a while
 
 int main()
 {
-    char s_1[] = {"☆ 岁月带走了年少轻狂 也慢慢沉淀了冷暖自知"};
-    char s_2[] = {"☆ 生活都有不尽人意 但依旧需要满怀信心"};
-    char s_3[] = {"☆ 迎新的笔写不出惆怅 在新的一年里"};
-    char s_4[] = {"☆ 愿你梦里星河灿烂满辰大海"};
-    char s_5[] = {"☆ 愿你踏破阻浪拥抱单纯美好"};
-    char s_6[] = {"☆ 愿你谈笑欢脱忘记琐碎烦恼"};
-    char s_7[] = {"☆ 愿你长乐未央终有良友相伴"};
-    char s_8[] = {"☆ 愿你执手他人诗酒相趁年华"};
-    char s_9[] = {"☆ 愿你风轻云淡不曾失去笑脸"};
-    char s_10[] = {"☆ 新的2020年 有新的期待和期许"};
-    char s_11[] = {"☆ 我在此用我单薄的言语"};
-    char s_12[] = {"☆ 祝各位 平安喜乐 得偿所愿"};
-    char s_13[] = {"☆  "};
-    char s_14[] = {"☆ 见字如面。"};
-    char s_15[] = {"☆ "};
-    char s_16[] = {"☆ "};
-    char s_17[] = {"                           2020春节快乐！\n"};
-    char s_18[] = {"                                                                YangGuangSheng"};
-    char s_19[] = {"                                                                 2020 New Year"};
+    // Sleep(3000); //Free up time to record
+    char s_1[] = {"\t\t\t\t\t\t\t☆ 岁月带走了年少轻狂 也慢慢沉淀了冷暖自知"};
+    char s_2[] = {"\t\t\t\t\t\t\t☆ 生活都有不尽人意 但依旧需要满怀信心"};
+    char s_3[] = {"\t\t\t\t\t\t\t☆ 迎新的笔写不出惆怅 在新的一年里"};
+    char s_4[] = {"\t\t\t\t\t\t\t☆ 愿你梦里星河灿烂满辰大海"};
+    char s_5[] = {"\t\t\t\t\t\t\t☆ 愿你踏破阻浪拥抱单纯美好"};
+    char s_6[] = {"\t\t\t\t\t\t\t☆ 愿你谈笑欢脱忘记琐碎烦恼"};
+    char s_7[] = {"\t\t\t\t\t\t\t☆ 愿你长乐未央终有良友相伴"};
+    char s_8[] = {"\t\t\t\t\t\t\t☆ 愿你执手他人诗酒相趁年华"};
+    char s_9[] = {"\t\t\t\t\t\t\t☆ 愿你风轻云淡不曾失去笑脸"};
+    char s_10[] = {"\t\t\t\t\t\t\t☆ 新的2020年 有新的期待和期许"};
+    char s_11[] = {"\t\t\t\t\t\t\t☆ 我在此用我单薄的言语"};
+    char s_12[] = {"\t\t\t\t\t\t\t☆ 祝各位 平安喜乐 得偿所愿"};
+    char s_13[] = {"\t\t\t\t\t\t\t☆  "};
+    char s_14[] = {"\t\t\t\t\t\t\t☆ 见字如面。"};
+    char s_15[] = {"\t\t\t\t\t\t\t☆ "};
+    char s_16[] = {"\t\t\t\t\t\t\t☆ "};
+    char s_17[] = {"\t\t\t\t\t\t\t                           2020春节快乐！\n"};
+    char s_18[] = {"\t\t\t\t\t\t\t                                                                YangGuangSheng"};
+    char s_19[] = {"\t\t\t\t\t\t\t                                                                 2020 New Year"};
     char *s_set[] = {s_1, s_2, s_3, s_4, s_5, s_6, s_7, s_8, s_9, s_10, s_11, s_12, s_13, s_14, s_15, s_16, s_17, s_18, s_19};
 
     char s[9] = "color "; //define a string to replace command "system("color xx")",so can easy change colors
@@ -38,70 +40,54 @@ int main()
     puts("\n\n\n\n\n\n ");      //Adjust the position when words printed
     for (int i = 0; i < N; i++) //make a flicker effect,shine N times
     {
-        s[6] = RandColor(); // background color
-        s[7] = RandColor(); // foreground color
-        system(s);          //Call cmd's color command
+        s[6] = RandBGColor(); // background color
+        s[7] = RandFGColor(); // foreground color
+        system(s);            //Call cmd's color command
 
-        puts("\r                                   ★ 2020春节快乐! ★");
+        puts("\t\t\t\t\t\t                                   ★ 2020春节快乐! ★");
         // Caution: Use printf() to replace puts(), the text will only be printed once
         for (int i = 0; i < 81474400; i++)
             ; //make the flicker effect softer and slower
     }
 
-    puts("\n\n亲爱的计安的小伙伴们:\n");
-    puts("\t新春快乐!\n");
-
-    s[6] = '4'; // change background color when diaplay the main words, 4--red
+    puts("\n\n\t\t\t\t\t\t\t亲爱的的小伙伴们:\n");
+    puts("\t\t\t\t\t\t\t\t新春快乐!\n");
 
     for (int i = 0; i < LINE; i++)
     {
-        puts("\n\a");       //Add sound(beep) when words come out
-        s[7] = RandColor(); //foreground color,change words color randomly
-        system(s);          //call cmd's color command
-        puts(*(s_set + i)); // Print words
-        Delay();            //delay for a while
+        puts("\n\a");         //Add sound(beep) when words come out
+        s[6] = '4';           // change background color when display the main words, 4--red
+        s[7] = RandFGColor(); //foreground color,change words color randomly
+        system(s);            //call cmd's color command
+        puts(*(s_set + i));   // Print words
+        Delay();              //delay for a while
     }
 
-    getchar(); //stop the program
+    getchar(); //pause the program
 
     return 0;
 }
 
-char RandColor() //Randomly generated colors
-{
-    char a = 0;
-    a = (char)abs(rand() % 16);
-    if (a >= 10)
-    {
-        a -= 10;
-        a += 97;
-    }
-    else
-        a += 48;
-
-    return a;
-}
-
-char RandBGColor(void) //use light colors
+char RandBGColor(void) //use light colors （color: 9 -- F)
 {
     char bgcolor;
 
     srand((unsigned)time(NULL));
-    bgcolor = rand() % 6 + 15;
-    bgcolor += 'a' - 10; //Decimal to Hexadecimal
+    bgcolor = rand() % 7 + 9;
+    bgcolor += 'A' - 10;
 
-    return (char)bgcolor;
+    return bgcolor;
 }
 
-char RandFGColor(void) //use colors
+char RandFGColor(void) //use normal colors (color: 0 -- 8)
 {
     char fgcolor;
 
     srand((unsigned)time(NULL));
-    fgcolor = rand() % 10 + 0;
-    //fgcolor += 'A' - 10;//Decimal to Hexadecimal
+    fgcolor = rand() % 9 + 0;
+    fgcolor += '0';
 
-    return (char)fgcolor;
+    return fgcolor;
 }
 
 void Delay(void)
