09806b6f83c6a134fdfd49a2440a875863a10c9e
guangshengyang <guangshengyang01@gmail.com>
2020-04-13 17:27:48 +0800

    Create stack folder

---
+++ 数据结构 School/栈/栈_链式实现.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+typedef int ElemType;
+typedef struct StackNode
+{
+    ElemType data;
+    StackNode *next;
+} StackNode;
+
+typedef struct
+{
+    StackNode *top; //栈顶指针
+} LinkStack;
+/*初始化链栈*/
+void InitStack(LinkStack *s)
+{
+    s->top = NULL;
+    printf("\n已经初始化链栈\n");
+}
+/*链栈置空*/
+void setEmpty(LinkStack *s)
+{
+    s->top = NULL;
+
+    printf("\n链栈被置空！\n");
+}
+/*入栈*/
+void pushLstack(LinkStack *s, ElemType x)
+{
+    StackNode *p;
+    p = (StackNode *)malloc(sizeof(StackNode));
+    p->data = x;
+    /*将未完成的代码补全,提示：此处添加一条语句*/
+    s->top = p;
+}
+/*出栈*/
+ElemType popLStack(LinkStack *s)
+{
+    ElemType x;
+    StackNode *p;
+    p = s->top;
+    if (s->top == 0)
+    {
+        printf("\n栈空，不能出栈！\n");
+        return (0);
+    }
+    x = p->data;
+    printf("\n当前出栈的数据是：%d", x);
+    s->top = p->next;
+    free(p);
+    return (x);
+}
+/*取栈顶元素*/
+ElemType StackTop(LinkStack *s)
+{
+    ElemType x;
+    if (s->top == 0)
+    {
+        printf("\n链栈空!\n");
+        return 0;
+    }
+    {
+        /*将未完成的代码补全,提示：此处添加一条语句*/
+        printf("\n\n当前链栈的栈顶元素为：%d\n", x);
+        return (x);
+    }
+}
+/*遍历链栈*/
+void Disp(LinkStack *s)
+{
+    printf("\n链栈中的数据为:\n");
+    printf("============================\n");
+    StackNode *p;
+    p = s->top;
+    while (p != NULL)
+    {
+        printf("%d\n", p->data);
+        p = p->next;
+    }
+    printf("============================\n");
+}
+void main()
+{
+    int i, n, a;
+    LinkStack *s;
+    s = (LinkStack *)malloc(sizeof(LinkStack));
+    int cord;
+    printf("第一次使用必须初始化！\n");
+
+    do
+    {
+        printf("\n");
+        printf("\n----------主菜单---------\n");
+        printf("\n---- -1   初始化链栈-----\n");
+        printf("\n------2   入栈-----------\n");
+        printf("\n------3   出栈-----------\n");
+        printf("\n------4   取栈顶元素-----\n");
+        printf("\n------5   置空链栈-------\n");
+        printf("\n------6   结束程序运行---\n");
+        printf("\n-------------------------\n");
+        printf("请从菜单栏中选择（1,2,3,4,5,6）");
+        scanf("%d", &cord);
+        printf("\n");
+        switch (cord)
+        {
+        case 1:
+        {
+            InitStack(s);
+            Disp(s);
+        }
+        break;
+        case 2:
+        {
+            printf("请输入将要压入链栈的数据个数:n=");
+            scanf("%d ", &n);
+            printf("依次将%d个数据压入链栈:\n", n);
+            for (i = 1; i <= n; i++)
+            {
+                scanf("%d", &a);
+                pushLstack(s, a);
+            }
+            Disp(s);
+        }
+        break;
+        case 3:
+        {
+            popLStack(s);
+            Disp(s);
+        }
+        break;
+        case 4:
+        {
+            StackTop(s);
+            printf("\n");
+        }
+        break;
+        case 5:
+        {
+            setEmpty(s);
+            Disp(s);
+        }
+        break;
+        case 6:
+            exit(0);
+        }
+    } while (cord <= 6);
+}
---
+++ 数据结构 School/栈/栈_顺序实现.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXNUM 20
+#define ElemType int
+typedef struct
+{
+    ElemType stack[MAXNUM];
+    int top;
+} SqStack;
+/*初始化顺序栈*/
+void InitStack(SqStack *p)
+{
+    if (!p)
+        printf("内存分配失败！");
+    p->top = -1;
+}
+/*入栈*/
+void Push(SqStack *p, ElemType x)
+{
+    if (p->top < MAXNUM - 1)
+    {
+        p->top = p->top + 1;
+        p->stack[p->top] = x;
+    }
+    else
+        printf("Overflow!\n");
+}
+/*出栈*/
+ElemType Pop(SqStack *p)
+{
+    ElemType x;
+    if (p->top >= 0)
+    {
+        /*将未完成的代码补全,提示：此处添加一条语句*/
+        printf("之前的栈顶数据元素 %d 已经被删除\n", p->stack[p->top]);
+        p->top = p->top - 1;
+        return (x);
+    }
+    else
+    {
+        printf("Underflow!\n");
+        return (0);
+    }
+}
+/*取栈顶元素*/
+ElemType GetTop(SqStack *p)
+{
+    ElemType x;
+    if (p->top >= 0)
+    {
+        /*将未完成的代码补全,提示：此处添加一条语句*/
+        printf("\n栈顶元素为:%d\n", x);
+        return (x);
+    }
+    else
+    {
+        printf("Underflow!\n");
+        return (0);
+    }
+}
+/*遍历顺序表*/
+void OutStack(SqStack *p)
+{
+    int i;
+    printf("\n");
+    if (p->top < 0)
+        printf("这是一个空栈！");
+    printf("\n");
+    for (i = p->top; i >= 0; i--)
+        printf("第 %d 个元数据元素为：%6d\n", i, p->stack[i]);
+}
+/*置空顺序表*/
+void setEmpty(SqStack *p)
+{
+    p->top = -1;
+}
+void main()
+{
+    SqStack *q;
+    int cord;
+    ElemType a;
+    printf("第一次使用必须初始化！\n");
+    do
+    {
+        printf("\n");
+        printf("\n-----------------------主菜单------------------\n");
+        printf("\n----------------1   初始化顺序栈---------------\n");
+        printf("\n----------------2   插入一个元素---------------\n");
+        printf("\n----------------3   删除栈顶元素---------------\n");
+        printf("\n----------------4   取栈顶元素-----------------\n");
+        printf("\n----------------5   置空顺序栈-----------------\n");
+        printf("\n----------------6   结束程序运行---------------\n");
+        printf("\n-----------------------------------------------\n");
+        printf("请从菜单栏中选择（1,2,3,4,5,6）");
+        scanf("%d", &cord);
+        printf("\n");
+        switch (cord)
+        {
+        case 1:
+        {
+            q = (SqStack *)malloc(sizeof(SqStack));
+            InitStack(q);
+            OutStack(q);
+        }
+        break;
+        case 2:
+        {
+            printf("请输入要插入的数据元素:a=");
+            scanf("%d", &a);
+            Push(q, a);
+            OutStack(q);
+        }
+        break;
+        case 3:
+        {
+            Pop(q);
+            OutStack(q);
+        }
+        break;
+        case 4:
+        {
+            GetTop(q);
+            OutStack(q);
+        }
+        break;
+        case 5:
+        {
+            setEmpty(q);
+            printf("\n顺序栈被置空!\n");
+            OutStack(q);
+        }
+        break;
+        case 6:
+            exit(0);
+        }
+    } while (cord <= 6);
+}
