d9b207cce11d72dd47726699df316b2ccbddee98
guangshengyang <guangshengyang01@gmail.com>
2020-05-04 00:33:31 +0800

    Update program

--- 数据结构 School/串/串.c
+++ 数据结构 School/串/串.c
@@ -5,17 +5,34 @@
 试利用字符串的基本运算实现这个替换操作。
 
 6-2 编写一个算法frequency，统计在一个输入字符串中各个不同字符出现的频度。用适当的测试数据来验证这个算法。 */
+// C program for Naive Pattern Searching algorithm 
+#include <stdio.h> 
+#include <string.h> 
 
-#include<stdio.h>
-#include<stdlib.h>
+void search(char* pat, char* txt) 
+{ 
+	int M = strlen(pat); 
+	int N = strlen(txt); 
 
-int main(void)
-{
+	/* A loop to slide pat[] one by one */
+	for (int i = 0; i <= N - M; i++) { 
+		int j; 
 
-    return 0;
-}
+		/* For current index i, check for pattern match */
+		for (j = 0; j < M; j++) 
+			if (txt[i + j] != pat[j]) 
+				break; 
 
-void replace (String &s, String &t, String &v)
-{
-    
-}
+		if (j == M) // if pat[0...M-1] = txt[i, i+1, ...i+M-1] 
+			printf("Pattern found at index %d \n", i); 
+	} 
+} 
+
+/* Driver program to test above function */
+int main() 
+{ 
+	char txt[] = "AABAACAADAABAAABAA"; 
+	char pat[] = "AABA"; 
+	search(pat, txt); 
+	return 0; 
+} 
