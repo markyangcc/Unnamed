6b477ad7f6632e8c9f0ffb9e90c733ea1b778ac4
guangshengyang <guangshengyang01@gmail.com>
2020-05-03 23:32:30 +0800

    Create 串.c

---
+++ 数据结构 School/串/串.c
@@ -0,0 +1,21 @@
+/*  6-1 字符串的替换操作replace (String &s, String &t, String &v)是指：
+若t是s的子串，则用串v替换串t在串s中的所有出现；若t不是s的子串，则串s不变。         
+例如，若串s为“aabbabcbaabaaacbab”，串t为“bab”，串v为“abdc”，则执行replace操作后，串s中的结果为“aababdccbaabaaacabdc”。
+ s= "我和我的祖国"；   t="祖国"； v=“家人” v替换t后，成为s=“我和我的家人”
+试利用字符串的基本运算实现这个替换操作。
+
+6-2 编写一个算法frequency，统计在一个输入字符串中各个不同字符出现的频度。用适当的测试数据来验证这个算法。 */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+
+    return 0;
+}
+
+void replace (String &s, String &t, String &v)
+{
+    
+}
