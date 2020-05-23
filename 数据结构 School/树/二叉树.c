b5dab88637e67f40c45f3a4a6553f2b4a9aa2f07
guangshengyang <guangshengyang01@gmail.com>
2020-05-23 17:45:53 +0800

    commit On master

--- 数据结构 School/树/二叉树.c
+++ 数据结构 School/树/二叉树.c
@@ -1,134 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-typedef struct BiTNode //定义二叉树节点结构
+struct Node
 {
-    char data;                       //数据域
-    struct BiTNode *lchild, *rchild; //左右孩子指针域
-} BiTNode, *BiTree;
+	int data;
+	struct Node* leftchild;
+	struct Node* rightchild;
+};
 
-void CreateBiTree(BiTree); //生成一个二叉树
-void PreOrder(BiTree);     //先序递归遍历二叉树
-void InOrder(BiTree);      //中序递归遍历二叉树
-void PostOrder(BiTree);    //后序递归遍历二叉树
+int insert(struct Node** root)
+{
+	struct Node* temp;
+
+	char data;
+	while (scanf("%c", &data) != EOF)
+	{
+		if (data == '\n')
+			break;
+
+		if (*root == NULL)
+		{
+			(temp) = malloc(sizeof(struct Node));
+			(temp)->data = data;
+			(temp)->leftchild = NULL;
+			(temp)->rightchild = NULL;
+			*root = temp;
+		}
+		else
+		{
+			return insert(&(*root)->leftchild);
+			return insert(&(*root)->rightchild);
+		}
+	}
+}
+
+void preorder(struct Node* root)
+{
+	if (root != NULL)
+	{
+		printf("%c-", root->data);
+		preorder(root->leftchild);
+		preorder(root->rightchild);
+	}
+}
+void inorder(struct Node* root)
+{
+	if (root != NULL)
+	{
+		preorder(root->leftchild);
+		printf("%c-", root->data);
+		preorder(root->rightchild);
+	}
+}
+void postorder(struct Node* root)
+{
+	if (root != NULL)
+	{
+		preorder(root->leftchild);
+		preorder(root->rightchild);
+		printf("%c-", root->data);
+	}
+}
 
 int main(void)
 {
-    BiTree T;
-    char ch, j;
-    int flag = 1;
-    bool temp;
-    //---------------------程序解说-----------------------
-    printf("本程序实现二叉树的操作。\n");
-    printf("可以进行建立二叉树，递归先序、中序、后序遍历等操作。\n");
-    //----------------------------------------------------
-    printf("请将先序遍历二叉树的结果输入以建立二叉树。\n");
-    printf("对于叶子结点以空格表示。\n");
-    printf("例如:abc  de g  f   (回车)，建立如下二叉树：\n");
-    printf("           a      \n");
-    printf("          /       \n");
-    printf("         b        \n");
-    printf("        / \\       \n");
-    printf("       c   d      \n");
-    printf("          / \\     \n");
-    printf("         e   f    \n");
-    printf("          \\       \n");
-    printf("           g      \n");
+	struct Node* root = NULL;
 
-    CreateBiTree(T); //初始化队列
+	insert(&root);
+	/* insert(&root, 'a');
+	insert(&root, 'b');
+	insert(&root, 'c');
+	insert(&root, ' ');
+	insert(&root, ' ');
+	insert(&root, 'd');
+	insert(&root, 'e');
+	insert(&root, ' ');
+	insert(&root, 'g');
+	insert(&root, ' ');
+	insert(&root, ' ');
+	insert(&root, 'f');
+	insert(&root, ' ');
+	insert(&root, ' ');
+	insert(&root, ' '); */
 
-    while (getchar() != '\n')
-        continue;
+	printf("preorder display :\n");
+	preorder(root);
+	printf("\ninorder display:\n");
+	inorder(root);
+	printf("\npostorder display:\n");
+	postorder(root);
 
-    while (flag)
-    {
-        printf("请选择: \n");
-        printf("1.递归先序遍历\n");
-        printf("2.递归中序遍历\n");
-        printf("3.递归后序遍历\n");
-        printf("4.退出程序\n");
-
-        scanf("%c", &j);
-
-        switch (j)
-        {
-        case '1':
-            if (T)
-            {
-                printf("先序遍历二叉树:");
-                PreOrder(T);
-                printf("\n");
-            }
-            else
-                printf("二叉树为空!\n");
-            break;
-        case '2':
-            if (T)
-            {
-                printf("中序遍历二叉树:");
-                InOrder(T);
-                printf("\n");
-            }
-            else
-                printf("二叉树为空!\n");
-            break;
-        case '3':
-            if (T)
-            {
-                printf("后序遍历二叉树:");
-                PostOrder(T);
-                printf("\n");
-            }
-            else
-                printf("二叉树为空!\n");
-            break;
-        default:
-            flag = 0;
-            printf("程序运行结束，按任意键退出!\n");
-        }
-    }
-    getchar();
-}
-
-void CreateBiTree(BiTree T)
-{
-    char ch;
-    scanf("%c", &ch); //读入一个字符
-    if (ch == ' ')
-        T = NULL;
-    else
-    {
-        T = (BiTNode *)malloc(sizeof(BiTNode)); //生成一个新结点
-        T->data = ch;
-        CreateBiTree(T->lchild); //生成左子树
-        CreateBiTree(T->rchild); //生成右子树
-    }
-}
-void PreOrder(BiTree T)
-{
-    if (T)
-    {
-        printf("%c", T->data); //访问结点
-        PreOrder(T->lchild);   /*将未完成的代码补全,提示：此处添加一条语句*/
-        PreOrder(T->rchild);   /*将未完成的代码补全,提示：此处添加一条语句*/
-    }
-}
-void InOrder(BiTree T)
-{
-    if (T)
-    {
-        InOrder(T->lchild);    /*将未完成的代码补全,提示：此处添加一条语句*/
-        printf("%c", T->data); //访问结点
-        InOrder(T->rchild);    /*将未完成的代码补全,提示：此处添加一条语句*/
-    }
-}
-void PostOrder(BiTree T)
-{
-    if (T)
-    {
-        PostOrder(T->lchild);  /*将未完成的代码补全,提示：此处添加一条语句*/
-        PostOrder(T->rchild);  /*将未完成的代码补全,提示：此处添加一条语句*/
-        printf("%c", T->data); //遍历右子树
-    }
+	return 0;
 }
