8a43642513d045ed91de69c3ca8deaa6e29c5286
guangshengyang <guangshengyang01@gmail.com>
2020-06-24 12:22:22 +0800

    rewrote rat in a maze problem silution, add dynamic display(2D) of the solving progress

--- 课程设计/maze.cpp
+++ 课程设计/maze.cpp
@@ -1,215 +1,253 @@
+#include <Windows.h>
 #include <iostream>
-#include <stdlib.h>
-#include <ctime>
-#include <windows.h>
 #include <stack>
 
 using namespace std;
-/* using std::cin;
-using std::cout;
-using std::endl;
-using std::stack; */
 
-typedef struct
+//Position定义坐标
+struct Position
 {
-    int row;
-    int col;
-} POSITION;
+	int row;
+	int col;
+};
 
-const POSITION maze_size = {30, 60};
-
-int **const maze = new int *[maze_size.row + 2];
-
-stack<POSITION> path;
-
-POSITION offset[4]; //direction
-
-void init();
-void randomMaze();
-bool findPath();
-void outputMaze();
-void setPathOnMaze();
-
-int main()
+//Maze类创建迷宫并寻找最短路径
+class Maze
 {
-    printf("%d", maze_size.row);
-    printf("\n-------------------------\n");
-    srand((unsigned int)time(NULL));
+private:
+	int row;  //迷宫的宽度
+	int col;  //迷宫的高度
+	int show; //1=演示寻径流程 或者 0=不演示寻径流程
 
-    init();
-    randomMaze();
+	Position offset[4]; //上下左右的偏移量
 
-    //打印随机生成的迷宫
-    /* for (int i = 0; i < maze_size.row; i++)
-	{
-		for (int j = 0; j < maze_size.col; j++)
+	stack<Position> path; //存储路径的堆栈
+	char **maze;		  //迷宫地图的数组
+	char **mazeShow;	  //用来显示迷宫寻径结果的数组
+
+public:
+	Maze(int row, int col, int show);
+	void ShowArray(char **array, int m, int n); //输出m*n的二维数组
+	bool FindPath();							//寻找迷宫出口的方法
+	void DrawPath();							//绘制迷宫的寻径结果
+	void ShowDetail(Position here);				//展示寻径的流程
+};
+
+//迷宫数据的输入和偏移量的初始化
+Maze::Maze(int row, int col, int show)
+{
+	this->row = row;
+	this->col = col;
+	this->show = show;
+
+	//初始化maze二维数组
+	maze = new char *[row + 2];
+	for (int i = 0; i < row + 2; ++i)
+		maze[i] = new char[col + 2];
+
+	//初始化mazeShow二维数组
+	mazeShow = new char *[row + 2];
+	for (i = 0; i < row + 2; ++i)
+		mazeShow[i] = new char[col + 2];
+
+	//对偏移量进行初始化
+	offset[0].row = 0;
+	offset[0].col = 1; //向右
+	offset[1].row = 1;
+	offset[1].col = 0; //向下
+	offset[2].row = 0;
+	offset[2].col = -1; //向左
+	offset[3].row = -1;
+	offset[3].col = 0; //向上
+
+	//输入迷宫的地图数据
+	int mazeData[10][10] =
 		{
-			printf("%2d", maze[i][j]);
+			{0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 1, 0, 1, 0, 0},
+			{0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+			{0, 1, 0, 1, 0, 1, 0, 1, 1, 0},
+			{0, 1, 0, 1, 0, 1, 0, 1, 0, 0},
+			{0, 1, 1, 1, 0, 1, 0, 1, 0, 1},
+			{0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+			{0, 0, 0, 1, 1, 0, 0, 1, 0, 0},
+			{1, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+			{0, 0, 0, 0, 1, 0, 0, 1, 0, 0},
+		};
+
+	//将迷宫数据导入迷宫数组中
+	for (int ii = 0; ii < row; ii++)
+	{
+		for (int jj = 0; jj < col; jj++)
+		{
+			maze[ii + 1][jj + 1] = mazeData[ii][jj];
 		}
-		printf("\n");
 	}
-	printf("\n====================\n"); */
 
-    cout << "Init Maze: " << endl;
-    outputMaze();
+	//在迷宫外增加一圈障碍物
+	for (int r = 0; r <= row + 1; r++)
+	{
+		maze[0][r] = maze[row + 1][r] = 1;
+	}
+	for (int c = 0; c <= col + 1; c++)
+	{
+		maze[c][0] = maze[c][col + 1] = 1;
+	}
 
-    cout << endl
-         << endl;
-
-    if (findPath())
-    {
-        cout << "Find a path:" << endl;
-        setPathOnMaze();
-        outputMaze();
-    }
-    else
-    {
-        cout << "No path" << endl;
-    }
-
-    return 0;
+	for (int iii = 0; iii < row + 2; iii++)
+	{
+		for (int jjj = 0; jjj < col + 2; jjj++)
+		{
+			mazeShow[iii][jjj] = maze[iii][jjj];
+		}
+	}
+	if (show)
+	{
+		for (int t = 0; t < 30; t++)
+			cout << endl;
+	}
+	cout << endl
+		 << "原始的迷宫地图：" << endl;
+	ShowArray(maze, row + 2, col + 2);
 }
 
-void init()
+//遍历输出二维数组
+void Maze::ShowArray(char **array, int m, int n)
 {
-    //偏移
-    offset[0].row = 0;
-    offset[0].col = 1; //right
-    offset[1].row = 1;
-    offset[1].col = 0; //down
-    offset[2].row = 0;
-    offset[2].col = -1; //left
-    offset[3].row = -1;
-    offset[3].col = 0; //up
-
-    for (int i = 0; i < maze_size.row + 2; i++) //利用一维数组创建二维数组
-    {
-        maze[i] = new int[maze_size.col + 2];
-    }
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << array[i][j] << " ";
+		}
+		cout << endl;
+	}
 }
 
-//地图范围1 - maze_size(从0开始) 有围墙
-void randomMaze()
+//显示具体的寻径流程
+void Maze::ShowDetail(Position here)
 {
-    int i, j, rate;
-
-    for (i = 0; i < maze_size.row + 2; i++)
-    {
-        for (j = 0; j < maze_size.col + 2; j++)
-        {
-            //设置围墙
-            if ((i == 0) || (i == maze_size.row + 1) || (j == 0) || (j == maze_size.col + 1))
-            {
-                maze[i][j] = 1;
-            }
-            else
-            {
-                rate = rand() % 10 + 1; //生成1-10之间的随机数
-                if (rate <= 2)          //迷宫的难度，数字越小难度越低
-                {
-                    maze[i][j] = 1; //随机生成障碍
-                }
-                else
-                {
-                    maze[i][j] = 0;
-                }
-            }
-        }
-    }
-    //保证起点与终点不存在障碍
-    maze[1][1] = maze[maze_size.row][maze_size.col] = 0;
+	Sleep(500);
+	for (int t = 0; t < 30; t++)
+		cout << endl;
+	char me = 2;
+	for (int i = 0; i < row + 2; i++)
+	{
+		for (int j = 0; j < col + 2; j++)
+		{
+			if (i == here.row && j == here.col)
+			{
+				cout << me << " ";
+			}
+			else
+			{
+				cout << maze[i][j] << " ";
+			}
+		}
+		cout << endl;
+	}
 }
 
-void outputMaze()
+//寻找从位置(1，1)到出口(m,m)的路径
+//如果成功则true，否则返回false
+bool Maze::FindPath()
 {
-    int i, j;
-    for (i = 0; i < maze_size.row + 2; i++)
-    {
-        for (j = 0; j < maze_size.col + 2; j++)
-        {
-            if (maze[i][j] == 1)
-            {
-                cout << "#";
-            }
-            else if ((maze[i][j] == 0) || (maze[i][j] == 3))
-            //else if (maze[i][j] == 0)
-            {
-                cout << " ";
-            }
-            else //为设置为非0，1，3的加颜色
-            {
-                HANDLE hOut;
-                hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-                SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-                cout << "#";
-                SetConsoleTextAttribute(hOut, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-            }
-        }
-        cout << endl;
-    }
+	Position here;
+	here.row = 1;
+	here.col = 1;
+
+	maze[1][1] = 1;
+
+	int option = 0;		//选择的移动方向
+	int LastOption = 3; //最后一个方向选择
+
+	//寻找一条路径
+	while (here.row != row || here.col != col)
+	{
+		//寻找并且移动到一个相邻的位置
+		int r, c;
+		while (option <= LastOption)
+		{
+			r = here.row + offset[option].row;
+			c = here.col + offset[option].col;
+
+			if (maze[r][c] == 0)
+				break;
+			option++;
+		}
+
+		//是否找到一个相邻的位置
+		if (option <= LastOption)
+		{
+			//移动到maze[r][c]
+			//将当前位置压入堆栈
+			path.push(here);
+			here.row = r;
+			here.col = c;
+
+			//设置障碍物以阻止再次访问
+			maze[r][c] = 4;
+			option = 0;
+		}
+		else
+		{
+			//没有可用的相邻位置，回溯上一个节点
+			if (path.empty())
+				return false;
+
+			Position next = path.top();
+			path.pop();
+
+			option = 0;
+
+			here = next;
+		}
+		if (show)
+		{
+			ShowDetail(here);
+		}
+	}
+	return true;
 }
 
-bool findPath()
+//绘制最终的寻径结果
+void Maze::DrawPath()
 {
-    POSITION here; //当前位置
-    here.row = here.col = 1;
+	Position temp;
+	while (!path.empty())
+	{
+		temp = path.top();
+		path.pop();
+		mazeShow[temp.row][temp.col] = '+';
+	}
 
-    maze[1][1] = 3; //放置障碍，防止回来
-    int option = 0; //next step
-    const int lastOption = 3;
+	mazeShow[row][col] = 3;
+	mazeShow[1][1] = 2;
 
-    //find a path
-    while (here.row != maze_size.row || here.col != maze_size.col)
-    {
-        //not reach the edge(maze_size是围墙)
-        int r, c;
-        while (option <= lastOption)
-        {
-            r = here.row + offset[option].row;
-            c = here.col + offset[option].col;
-            if (maze[r][c] == 0)
-            {
-                break;
-            }
-            option++; //next choice
-                      //outputMaze();
-                      //printf("\n\n");
-        }
-
-        //相邻的位置能走？
-        if (option <= lastOption)
-        {
-            path.push(here);
-            here.row = r;
-            here.col = c;
-            maze[r][c] = 3; //走过了，设置为障碍
-            option = 0;     //重置option
-        }
-        else
-        {
-            if (path.empty()) //栈空，证明无路可走了
-            {
-                return false;
-            }
-            //go back
-            maze[here.row][here.col] = 3; //此路不可通
-            path.pop();                   //TODO:优化而添加的代码
-            here = path.top();            //这里可以优化，栈顶的元素已经证明是死路，不需要在测试一遍了，直接pop()掉即可
-            path.pop();
-            option = 0;
-        }
-    }
-    maze[maze_size.row][maze_size.col] = 2; //不要设置为0，1，3代表特殊含义的数字都行
-
-    return true;
+	if (show)
+	{
+		for (int t = 0; t < 30; t++)
+			cout << endl;
+	}
+	cout << endl
+		 << "寻径后的迷宫地图：" << endl;
+	ShowArray(mazeShow, row + 2, col + 2);
 }
-void setPathOnMaze() //后面为栈里面的加颜色
+
+void main()
 {
-    POSITION pos;
-    while (!path.empty())
-    {
-        pos = path.top();
-        path.pop();
-        maze[pos.row][pos.col] = 2;
-    }
+	cout << "欢迎来到迷宫老鼠1.0版本!\n是否需要显示迷宫寻径的具体流程？(Y/N)" << endl;
+	char input;
+	cin >> input;
+	int show; //是否展示迷宫寻径流程，1展示，0不展示
+	if (input == 'Y' || input == 'y')
+		show = 1;
+	else if (input == 'N' || input == 'n')
+		show = 0;
+	else
+		return;
+
+	Maze *myMaze = new Maze(10, 10, show);
+	myMaze->FindPath();
+	myMaze->DrawPath();
 }
